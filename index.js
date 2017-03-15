5a07ad821cee28b64b49785eedcb402411bfd1f3
vuhoangha <vuhoangha100995@gmai.com>
2017-03-15 22:08:38 +0700

    x

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
-  res.send('xin chao');
+  res.send('xin chao 1');
 });
 
 app.listen(app.get('port'), function() {
